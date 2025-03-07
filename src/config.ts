57f58ba92ebddcde916e3d0471144f01942ab77a
SohamMhatre09 <ichbinsoham@gmail.com>
2025-03-07 08:47:40 +0530

    done

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 // API configuration
-export const API_URL = "https://datathon-r2-epcchmfucpc3f0a0.canadacentral-01.azurewebsites.net";
+export const API_URL = "datathon-r2-epcchmfucpc3f0a0.canadacentral-01.azurewebsites.net";
